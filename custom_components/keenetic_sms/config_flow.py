79750ee7257ae3eb7801d7d41e2100f6064a633e
Leonid <77241781+LeoI5@users.noreply.github.com>
2025-07-11 21:18:00 +0300

    Update config_flow.py

--- custom_components/keenetic_sms/config_flow.py
+++ custom_components/keenetic_sms/config_flow.py
@@ -1,7 +1,20 @@
+import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.data_entry_flow import FlowResult
 
-class KeeneticSMSConfigFlow(config_entries.ConfigFlow, domain="keenetic_sms"):
-    async def async_step_user(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title="Keenetic SMS", data={})
-        return self.async_show_form(step_id="user")
+from .const import DOMAIN
+
+class KeeneticSMSConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None) -> FlowResult:
+        if user_input:
+            return self.async_create_entry(title="Keenetic SMS", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required("host", default="http://192.168.0.1:81"): str,
+                vol.Optional("interval", default=60): int,
+            }),
+        )
